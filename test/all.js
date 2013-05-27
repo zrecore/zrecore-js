9df6a2ffe01019194b9b456f7365d6517a5d18a7
Alex Albino <webmaster@alexventure.com>
2013-05-27 16:24:50 -0700

    adding preliminary test files

--- test/all.js
+++ test/all.js
@@ -1,7 +1,137 @@
 /**
- * Created with JetBrains WebStorm.
- * User: aalbino
- * Date: 5/27/13
- * Time: 12:01 PM
- * To change this template use File | Settings | File Templates.
+ * acl-permission
+ */
+//exports['test acl-permission'] = require('./test-acl-permission');
+
+/**
+ * acl-resource
+ */
+//exports['test /acl-resource'] = require('./test-acl-resource');
+
+/**
+ * acl-role
+ */
+
+/**
+ * category
+ */
+
+/**
+ * comment
+ */
+
+/**
+ * coupon
+ */
+
+/**
+ * currency
+ */
+
+/**
+ * folder
+ */
+
+/**
+ * item
+ */
+
+/**
+ * item-coupon
+ */
+
+/**
+ * item-property
+ */
+
+/**
+ * merchant-gateway
+ */
+
+/**
+ * order
+ */
+
+/**
+ * order-coupon
+ */
+
+/**
+ * order-item
+ */
+
+/**
+ * order-service
+ */
+
+/**
+ * order-status-history
+ */
+
+/**
+ * order-subscription
+ */
+
+/**
+ * package-level
+ */
+
+/**
+ * page
+ */
+
+/**
+ * property
+ */
+
+/**
+ * property-type
+ */
+
+/**
+ * property-value
+ */
+
+/**
+ * service
+ */
+
+/**
+ * service-coupon
+ */
+
+/**
+ * service-package-level
+ */
+
+/**
+ * status
+ */
+
+/**
+ * subscription
+ */
+
+/**
+ * subscription-coupon
+ */
+
+/**
+ * subscription-package-level
+ */
+
+/**
+ * subscription-service
+ */
+
+/**
+ * user
+ */
+
+/**
+ * user-acl
+ */
+
+/**
+ * user-profile
  */

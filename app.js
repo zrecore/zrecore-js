5426821ebc312067104117227fc8c12261423490
zrecommerce <webmaster@alexventure.com>
2013-06-15 14:47:08 -0700

    Adding in authentication mechanism.

--- app.js
+++ app.js
@@ -14,11 +14,13 @@ var sslKeyPath = '';
 var sslCaPath = '';
 var databaseHost = 'localhost';
 var databaseName = 'zrecore';
+var authorizationRequired = false; // Set to false while you add your first User, then set back to true and restart the app.
 
 var http = require('http')
   , path = require('path')
   , fs = require('fs')
-  , restify = require('restify');
+  , restify = require('restify')
+  , _ = require('underscore');
 
 var ZRECORE_DIR = __dirname;
 var crud = require(ZRECORE_DIR + '/lib/crud.js');
@@ -47,7 +49,42 @@ mongoose.connect('mongodb://' + databaseHost + '/' + databaseName);
  * Set up our REST controllers.
  */
 
+// ...Enforce API authentication
 server.use(restify.bodyParser());
+
+if (authorizationRequired) {
+server.pre(function (req, res, next) {
+    var api_user = req.header('API-USER');
+    var api_key = req.header('API-KEY');
+
+    if ( _.isUndefined( api_user ) || _.isUndefined(api_key) ) {
+
+        res.send(401, '401 Access Denied');
+    } else {
+
+        // ...Attempt to look it up.
+        var User = require('./models/User.js');
+
+        var authUser = User.findOne({"handle": api_user}, function (err, user) {
+            if (user) {
+
+                user.comparePassword(api_key, function (err, isMatch) {
+                    if (err || !isMatch) {
+                       res.send(401, '401 Access Denied');
+                    } else {
+                        // OK! ...Just continue execution.
+                        next();
+                    }
+                });
+            } else {
+                // User not found
+                res.send(401, '401 Access Denied');
+            }
+        });
+    }
+
+});
+}
 
 var routeFiles = fs.readdirSync( ZRECORE_DIR + '/routes');
 var route = '';
--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 var mongoose = require("mongoose"),
     Schema = mongoose.Schema,
-    ObjectId = Schema.ObjectId;
+    ObjectId = Schema.ObjectId,
+    bcrypt = require('bcrypt'),
+    SALT_WORK_FACTOR = 10;
 
 var UserSchema = new Schema({
     "id": ObjectId,
@@ -55,6 +57,34 @@ var UserSchema = new Schema({
 }, {
     "autoIndex": false
 });
+
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password_hash')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password_hash, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password_hash = hash;
+            next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password_hash, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 UserSchema.index({
     "acl_role_id": 1,
